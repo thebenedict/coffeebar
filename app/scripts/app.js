e35fdad0dc8135a319b8cc1e39fbe75654b3f9c7
Michael Benedict <thebenedict@gmail.com>
2013-10-10 13:21:04 +0200

    fixed path

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,7 @@ angular.module('coffeebarApp', ['firebase', 'ui.router'])
     $stateProvider
       .state('/', {
         url: '',
-        templateUrl: '/views/main.html',
+        templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       });
   });
